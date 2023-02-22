eae0f6ed7ed171d33b4723efece10884ff23fa24
SAMUEL BERNARDO <samuel.hagers.bernardo@gmail.com>
2023-02-22 19:27:43 -0300

    Terminando

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [nome,setNome] = useState('')
+  const [numero,setNumero] = useState(0)
 
+  const escondaOZero = (numero) =>{
+    if(numero === 0) return "";return numero
+  }
+  const soma = () => {
+    setNumero((prevNumber) =>prevNumber +1)
+
+  }
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h2 className='Digite_Nome'>Digite seu nome</h2>
+      <h2 className='Seu_Nome'>{nome}</h2>
+      <input value={nome} onChange={e=>setNome(e.target.value)} className='Input_Nome' ></input>
+      <h2>Contator de clicks</h2>
+      <h2>{escondaOZero(numero)}</h2>
+      <button onClick={soma} className='Btn_contador'>Contando</button>
     </div>
   )
 }
